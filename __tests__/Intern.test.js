6a1603656f5ef9c3aac3c5bcbabc62493a3577b3
Aaron Deas <deas.aaron@gmail.com>
2021-06-09 19:35:45 -0400

    Wrote intern tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,15 +1,17 @@
 // Starter file provided by Instructor (03/09/2021) AC
-
 const Intern = require("../lib/Intern");
-
+const testIntern = new Intern("Tammy", 12, "tammy@tammy.com", "UNC");
 test("Can set school via constructor", () => {
-
+	const obj = testIntern;
+	expect(obj.school).toEqual("UNC");
 });
 
-test("getRole() should return \"Intern\"", () => {
-
+test('getRole() should return "Intern"', () => {
+	const obj = testIntern;
+	expect(obj.getRole()).toEqual("Intern");
 });
 
 test("Can get school via getSchool()", () => {
-
+	const obj = testIntern;
+	expect(obj.getSchool()).toEqual("UNC");
 });
