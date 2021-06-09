ff5b567153a34a44eb8d5e852fb496c2f80cc5d6
Aaron Deas <deas.aaron@gmail.com>
2021-06-09 19:47:01 -0400

    Added tests for managers

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,15 +2,18 @@
 
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
-
+const testMgr = new Manager("Rita", "33", "rita@rita.com", "100");
 test("Can set office number via constructor argument", () => {
-
+	const obj = testMgr;
+	expect(obj.officeNum).toEqual("100");
 });
 
 test('getRole() should return "Manager"', () => {
-
+	const obj = testMgr;
+	expect(obj.getRole()).toEqual("Manager");
 });
 
 test("Can get office number via getOffice()", () => {
-
+	const obj = testMgr;
+	expect(obj.getOffice()).toEqual("100");
 });
