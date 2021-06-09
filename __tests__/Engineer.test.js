9fe3e8799b536e9d75a418ec20e6ace677fb51a2
Aaron Deas <deas.aaron@gmail.com>
2021-06-09 19:16:06 -0400

    Finished tests for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,15 +1,24 @@
 // Starter file provided by Instructor (03/09/2021) AC
 
 const Engineer = require("../lib/Engineer");
-
+const testEng = new Engineer(
+	"Bob",
+	12,
+	"bob@bob.com",
+	"Employee",
+	"http://www.github.com/bobomb"
+);
 test("Can set GitHUb account via constructor", () => {
-
+	const obj = testEng;
+	expect(obj.github).toEqual("http://www.github.com/bobomb");
 });
 
-test("getRole() should return \"Engineer\"", () => {
-
+test('getRole() should return "Engineer"', () => {
+	const obj = testEng;
+	expect(obj.getRole()).toEqual("Engineer");
 });
 
 test("Can get GitHub username via getGithub()", () => {
-
+	const obj = testEng;
+	expect(obj.getGithub()).toEqual("bobomb");
 });
